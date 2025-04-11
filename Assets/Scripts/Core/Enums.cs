f34361060b68497e523babd0c7c50503b1751333
zaguarman <zaguarman@gmail.com>
2025-04-11 13:26:18 +0200

    Deleted TestSetup as its irrelevant now

--- Assets/Scripts/Core/Enums.cs
+++ Assets/Scripts/Core/Enums.cs
@@ -4,5 +4,5 @@ public class Enums {
     public enum EffectType { Immediate, Triggered, Continuous }
     public enum EffectTrigger { OnPlay, OnDeath, OnDamage, StartOfTurn, EndOfTurn }
     public enum ActionType { Damage, Heal, Draw, Summon, Armor, Stun, Buff }
-    public enum TargetType { Player, Enemy, AllCreatures, FriendlyCreatures, EnemyCreatures, Self, RandomAlly }
+    public enum TargetType { Player, Enemy, AllCreatures, FriendlyCreatures, EnemyCreatures, Self, RandomAlly, RandomEnemy }
 }
